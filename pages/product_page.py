363f0af1f2759ecccaab85ceface6040bc57ef0a
Prikol9 <nikset1@gmail.com>
2023-06-20 15:01:54 +0300

    test guest can add product to basket

---
+++ pages/product_page.py
@@ -0,0 +1,9 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+
+    def add_to_basket(self):
+        button_basket = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        button_basket.click()
